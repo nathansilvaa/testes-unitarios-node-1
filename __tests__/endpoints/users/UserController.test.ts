3607675f79b043bb683c1bf3e29cf9d1a87e242d
Nathan Aguiar Silva <nathansilvaxis@gmail.com>
2024-09-10 17:03:17 -0300

    Primeiro commit - testes unitários para o projeto

--- __tests__/endpoints/users/UserController.test.ts
+++ __tests__/endpoints/users/UserController.test.ts
@@ -60,4 +60,47 @@ describe('UserController', () => {
     expect(response.body.success).toBe(true);
     expect(response.body.data).toEqual(expectedUsers);
   });
+
+  it('Deve retornar 404 se o usuário não for encontrado', async () => {
+    jest.spyOn(UserRepository.prototype, 'findOne').mockReturnValueOnce(undefined);
+
+    const response = await request(app).get('/users/1');
+    expect(response.status).toBe(404);
+    expect(response.body.success).toBe(false);
+    expect(response.body.data).toBe('Usuário não encontrado');
+  });
+
+
+  it('Deve retornar erro 500 se a criação falhar', async () => {
+    const newUser: IUser = {
+      id: 7,
+      name: 'Sasukee',
+      age: 19,
+    };
+
+    // Mock do método save para simular uma falha ao criar o usuário
+    jest.spyOn(UserRepository.prototype, 'save').mockReturnValueOnce(false);
+
+    const response = await request(app).post('/users').send(newUser);
+
+    // Verificar se o status e o corpo da resposta estão corretos
+    expect(response.status).toBe(500);
+    expect(response.body.success).toBe(false);
+    expect(response.body.data).toBe('Falha ao criar o usuário');
+  });
+
+  it('Deve excluir um usuário com sucesso', async () => {
+    const userId = 1;
+
+    // Mock do método delete do UserRepository para simular que o usuário foi excluído com sucesso
+    jest.spyOn(UserRepository.prototype, 'delete').mockReturnValueOnce(true);
+
+    const response = await request(app).delete(`/users/${userId}`);
+
+    // Verificar se o status e o corpo da resposta estão corretos
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(true);
+    expect(response.body.data).toBe('Usuário excluído com sucesso');
+  });
 });
+ 
